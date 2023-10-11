89f5dd745c3328893024b8ecbe7f020277f98f6f
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-10-10 21:54:10 -0600

    Making the header responsive (in progress)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../Images/colibri_cabeza.png";
 
 const Header = () => {
   return (
-    <header className="Encabezado ">
+    <header className="Encabezado h-40 text-7xl 2xl:h-24 2xl:text-5xl xl:h-28 xl:text-5xl lg:h-28 lg:text-5xl">
       <button className="mx-3 my-1 w-24">
         {/* Boton de inicio, regresará hasta arriba */}
         <img src={logo} className="Logo" alt="Logo del Salón Colibrí"></img>
