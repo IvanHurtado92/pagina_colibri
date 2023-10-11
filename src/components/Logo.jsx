8146d4e0b55c2600516d9658097350549a036ebe
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-10-11 11:24:00 -0600

    Colors and responsiveness corrections

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,10 +5,10 @@ const Logo = () => {
   return (
     <div className="m-20">
       <div className="justify-center flex">
-        <img src={logo} className="colibri" alt="Logo del Salón Colibrí"></img>
+        <img src={logo} className="colibri h-auto 2xl:w-10" alt="Logo del Salón Colibrí"></img>
       </div>
 
-      <b className="text-slate-50 justify-center flex text-8xl brighter md:text-8xl lg:text-7xl xl:text-7xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700">
+      <b className="text-slate-50 justify-center flex text-3xl brighter md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700 md:text-yellow-400">
         Salón de Fiestas Infantiles Colibrí
       </b>
     </div>
