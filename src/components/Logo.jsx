cb4d32654999455bc1bd5fcf669f0bfb122cfa1d
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-10-10 23:11:46 -0600

    Small salon title responsivness

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
         <img src={logo} className="colibri" alt="Logo del Salón Colibrí"></img>
       </div>
 
-      <b className="text-slate-50 justify-center flex text-8xl brighter md:text-8xl lg:text-8xl xl:text-7xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700">
+      <b className="text-slate-50 justify-center flex text-8xl brighter md:text-8xl lg:text-7xl xl:text-7xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700">
         Salón de Fiestas Infantiles Colibrí
       </b>
     </div>
