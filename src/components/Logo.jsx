78a36b074002934a9671300a096f32f4bf5cea50
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-10-11 12:06:02 -0600

    Logo salón arreglado

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="m-20">
       <div className="justify-center flex">
-        <img src={logo} className="colibri h-auto 2xl:w-10" alt="Logo del Salón Colibrí"></img>
+        <img src={logo} className="colibri" alt="Logo del Salón Colibrí"></img>
       </div>
 
       <b className="text-slate-50 justify-center flex text-3xl brighter md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700 md:text-yellow-400">
