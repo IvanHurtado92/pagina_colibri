56e87ddb3882899bf425ec8ffee911bfcf898e41
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-10-11 18:29:30 -0600

    Fixing the title centering and the logo responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "../Images/colibri_cabeza.png";
 
 const Header = () => {
   return (
-    <header className="Encabezado h-12 text-2xl md:h-14 md:text-3xl 2xl:h-24 2xl:text-4xl xl:h-20 xl:text-3xl s">
-      <button className="mx-3 my-1 w-24">
+    <header className="Encabezado h-12 text-2xl  2xl:h-24 2xl:text-4xl xl:h-20 xl:text-3xl lg:h-18 lg:text-2xl md:h-14 md:text-3xl">
+      <button className="mx-4 my-1 w-24">
         {/* Boton de inicio, regresará hasta arriba */}
         <img src={logo} className="Logo" alt="Logo del Salón Colibrí"></img>
       </button>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,16 @@ import "../styles/Logo.css";
 
 const Logo = () => {
   return (
-    <div className="m-20">
+    <div className="m-20 text-center">
       <div className="justify-center flex">
-        <img src={logo} className="colibri" alt="Logo del Salón Colibrí"></img>
+        <img
+          src={logo}
+          className="colibri h-auto w-5/6 2xl:w-5/12 xl:w-5/12 lg:w-6/12 md:w-7/12"
+          alt="Logo del Salón Colibrí"
+        ></img>
       </div>
 
-      <b className="text-slate-50 justify-center flex text-3xl brighter md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700 md:text-yellow-400">
+      <b className="text-slate-50 text-3xl brighter md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 2xl:text-emerald-400 xl:text-blue-500 lg:text-red-700 md:text-yellow-400">
         Salón de Fiestas Infantiles Colibrí
       </b>
     </div>
