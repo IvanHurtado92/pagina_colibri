80b0718ee0435dbc643758ae0af93152a07c9edf
Ivan Hurtado <ivan.hurtado92@outlook.com>
2024-02-01 19:10:39 -0600

    Change: HambComp active state added

--- src/components/HambComp.jsx
+++ src/components/HambComp.jsx
@@ -4,7 +4,7 @@ import "../styles/HambComp.css"
 
 const HambComp = ({nombre, funcion}) => {
     return(
-        <button className="Hamb text-center px-1 py-1 w-full hover:bg-white" onClick={(event) => {
+        <button className="Hamb text-center px-1 py-1 w-full hover:bg-white active:bg-gray-300" onClick={(event) => {
             event.stopPropagation();
             console.log({nombre})}}>
             {nombre}
