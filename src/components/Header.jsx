d5315e3f91d457764027b7b0e9a271c8b82ce7aa
Ivan Hurtado <ivan.hurtado92@outlook.com>
2024-02-13 12:43:53 -0600

    Fix: Deployment menu fixed, Now shows and hides correctly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import "../styles/Header.css";
 import logo from "../Images/colibri_cabeza.png";
 import HambComp from "./HambComp";
 import { useState } from "react";
-import OutsideClickHandler from 'react-outside-click-handler';
 
 const componentes = [
   { nombre: "Fotos", funcion: null },
@@ -13,17 +12,15 @@ const componentes = [
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [hambClickeado, setHambClickeado] = useState(false);
 
   const abrirMenu = () => {
     setIsMenuOpen(!isMenuOpen); //cada que se usa el método set, el componente se recarga con la variable cambiada y todos los cambios al componente que involucre
-    setHambClickeado(!hambClickeado);
-    console.log("IsMenu ---> Menu abierto: " + !isMenuOpen + ", Hamburguesa Clickeada: " + !hambClickeado);
+    // console.log("IsMenu ---> Menu abierto: " + !isMenuOpen + ", Hamburguesa Clickeada: " + !hambClickeado);
   };
 
   const cerrarMenu = () => {
-    setHambClickeado(false);
-    console.log("CerraMenu ---> Menu abierto: " + isMenuOpen + ", Hamburguesa Clickeada: " + hambClickeado);
+    if (isMenuOpen) setIsMenuOpen(false);
+    // console.log("CerraMenu ---> Menu abierto: " + isMenuOpen + ", Hamburguesa Clickeada: " + hambClickeado);
   }
 
 
@@ -32,19 +29,19 @@ const Header = () => {
   return (
     <div className="sticky top-0 z-20">
       <header className="Encabezado h-12 text-2xl 2xl:h-24 2xl:text-4xl xl:h-20 xl:text-3xl lg:h-18 lg:text-2xl md:h-14 md:text-3xl">
-        <button className="mx-4 my-1 w-24">
+        <button className="mx-4 my-1 w-24 z-40">
           {/* Boton de inicio, regresará hasta arriba */}
           <img id="TopLogo" src={logo} className="Logo" alt="Logo del Salón Colibrí"></img>
         </button>
 
         {componentes.map((comp) => {
-          return <button className="Rm Titulo">{comp.nombre}</button>;
+          return <button className="Rm Titulo z-40">{comp.nombre}</button>;
         })}
 
         <button
           name="Hamburguesa"
           id="menu"
-          className="Titulo absolute inset-y-0 right-0 text-4xl"
+          className="Titulo absolute inset-y-0 right-0 text-4xl z-40"
           style={{ height: "50%", color:"white" }}
           onClick={abrirMenu}
         >
@@ -52,12 +49,12 @@ const Header = () => {
         </button>
 
       </header>
-
-      <OutsideClickHandler onOutsideClick={cerrarMenu}>
+        <div className='fixed top-0 left-0 w-full h-full z-0' onClick={cerrarMenu}></div> 
+        {/* El div superior se encarga de cerrar el menú hamburguesa cuando ya está desplegado */}
         <div
           id="DivHamb"
           className="z-10 absolute right-0 md:top-14 sm:top-12 border-black min-h-fit min-w-fit text-lg 2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-lg overflow-hidden"
-          style={{"opacity": (isMenuOpen && hambClickeado) ? 1 : 0}}
+          style={{"opacity": (isMenuOpen) ? 1 : 0}}
         >
           {/* Acomodar el menu usando anchor en cuanto ya sea funcional en los navegadores */}
           <div className="block text-center">
@@ -71,7 +68,6 @@ const Header = () => {
             })}
           </div>
         </div>
-      </OutsideClickHandler>
     </div>
   );
 };
